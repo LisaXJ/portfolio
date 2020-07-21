aca401623a60f6cca3f79dbd201612c4aaf1e69a
Xu <lisaxu@deloitte.ie>
2020-07-21 12:40:12 +0100

    add navigation bar and empty home page

---
+++ frontend/src/screens/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div className="nav-container">
+      <div className="main-heading">Hello</div>
+
+      <div className="menu-tag-container">
+		<div className="menu-tags">JavaScript</div>
+		<div className="menu-tags">MySQL</div>
+		<div className="menu-tags">Python</div>
+		<div className="menu-tags">Java</div>
+		<div className="menu-tags">Natural Language Processing</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
