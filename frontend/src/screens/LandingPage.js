a206cdeef1606c8fd5950b47b329b64e09837be8
Xu <lisaxu@deloitte.ie>
2020-07-22 10:01:17 +0100

    [ADD] Exit animations

--- frontend/src/screens/LandingPage.js
+++ frontend/src/screens/LandingPage.js
@@ -5,25 +5,35 @@ import Sidebar from './Sidebar';
 
 const LandingPage = () => {
     const [showLandingPage, setShowLandingPage] = useState(true);
+    const [buttonClicked, setButtonClicked] = useState(false);
 
     const toggle = () => {
         setShowLandingPage(false)
+        setButtonClicked(true);
     }
 
     return(
         <Fragment>
             { 
                 showLandingPage
-                ? <div className="landing-page-container overlay animate__animated animate__backInUp">
+                ? <div className={'landing-page-container overlay animate__animated animate__backInUp' + (buttonClicked ? ' animate__backOutUp' : '')}>
                         <div className="landing-page-text">
                             <p>Some text. And some more <u>text</u>.</p>
                             <Button className="lp-button button-animation-hidden" onClick={toggle}>Click Me!</Button>
                         </div>
                     </div>
 
-                : <div className="main-container animate__animated animate__fadeInUp fast">
-                    <Navigation  />
-                    <Sidebar />
+                :  <div className="main-container">
+                    <div className="animate__animated animate__backInUp animate__slow">
+                        <Navigation  />
+                        <Sidebar />
+                    </div>
+                    <div className={'landing-page-container overlay animate__animated animate__backOutUp'}>
+                        <div className="landing-page-text">
+                            <p>Some text. And some more <u>text</u>.</p>
+                            <Button className="lp-button button-animation-hidden" onClick={toggle}>Click Me!</Button>
+                        </div>
+                    </div>
                 </div>
             }
         </Fragment>
